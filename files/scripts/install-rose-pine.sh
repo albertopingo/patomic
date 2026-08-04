f9764a1259f0f407c497e600f5012615f3bc3298
Alberto Pingo <contact@albertopingo.com>
2026-08-04 03:57:19 +0100

    feat(theme): Add Rose Pine Icons

---
+++ files/scripts/install-rose-pine.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+echo "Installing Rose Pine icons..."
+
+THEME_NAME="Rosepine-Dark"
+BUILD_DIR=$(mktemp -d)
+ICONS_URL="https://github.com/rose-pine/gtk/releases/latest/download/rose-pine-icons.tar.gz"
+# THEME_GTK3_URL="https://github.com/rose-pine/gtk/releases/latest/download/gtk3.tar.gz"
+# THEME_GTK4_URL="https://github.com/rose-pine/gtk/releases/latest/download/gtk4.tar.gz"
+
+curl -Lo "$BUILD_DIR/rose-pine-icons.tar.gz" "$ICONS_URL"
+mkdir -p /usr/share/icons
+tar -xzf "$BUILD_DIR/rose-pine-icons.tar.gz" -C /usr/share/icons
+
+rm -rf "$BUILD_DIR"
